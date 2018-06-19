4a3df331788d462f08ff4c0e72269670f4c3dce8
Victor D <chuvidi2003@gmail.com>
2018-06-19 11:32:01 -0400

    file delete

--- utils/credentials.py
+++
@@ -1,4 +0,0 @@
-flickr_key = "e7eddc34a375e2784f4df2ab865446ff"
-flickr_secret = "46121dcd8b2890cb"
-
-google_app_clientid = "396732791703-i8p267n53u17avid8vutq2kfi72eh5bb.apps.googleusercontent.com"
