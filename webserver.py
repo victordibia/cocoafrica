c7941fe366edb35fecd2b4afb82bc7a9ee310626
Victor Dibia <chuvidi2003@gmail.com>
2018-08-22 09:53:03 -0400

    ui update

--- webserver.py
+++ webserver.py
@@ -8,50 +8,67 @@ from utils import datastore as ds
 from utils import auth
 app = Flask(__name__, )
 
-
 flickr.init(credentials)
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
-@app.route("/verifytoken",methods=['POST'])
+
+@app.route("/verifytoken", methods=['POST'])
 def verifytoken():
-    input_data = request.json 
-    response_payload = {"status": auth.verify_token(input_data["token"], credentials.google_app_clientid)}
+    input_data = request.json
+    response_payload = {
+        "status":
+        auth.verify_token(input_data["token"], credentials.google_app_clientid)
+    }
     return jsonify(response_payload)
 
 
 @app.route("/exploredataset")
 def exploredataset():
-    data_results  = ds.get_images()
+    data_results = ds.get_images()
     for row in data_results:
-        result_payload = {"url": row["url"], "title": "", "source": row["source"], "description":row["description"]}
+        result_payload = {
+            "url": row["url"],
+            "title": "",
+            "source": row["source"],
+            "description": row["description"]
+        }
         # print(result_payload)
 
     return jsonify(result_payload)
+
 
 @app.route("/coco")
 def test():
     return render_template('index.html')
 
-@app.route("/search",methods=['POST'])
+
+@app.route("/search", methods=['POST'])
 def search():
-    input_data = request.json 
+    input_data = request.json
     print(input_data)
-    response_payload = {"searchresults": flickr.search_photos_text(input_data["searchtext"], input_data["numresults"], input_data["page"], input_data["sort"] )}
+    response_payload = {
+        "searchresults":
+        flickr.search_photos_text(input_data["searchtext"],
+                                  input_data["numresults"], input_data["page"],
+                                  input_data["sort"])
+    }
     # print(response_payload)
     return jsonify(response_payload)
 
 
-@app.route("/curateimage",methods=['POST'])
+@app.route("/curateimage", methods=['POST'])
 def curate():
-    input_data = request.json 
-    print(input_data)  # print(response_payload) 
-    save_status = ds.save_image_data(flickr.get_image_info(input_data["id"]), input_data)
+    input_data = request.json
+    print(input_data)  # print(response_payload)
+    save_status = ds.save_image_data(
+        flickr.get_image_info(input_data["id"]), input_data)
     response_payload = {"status": save_status}
     return jsonify(response_payload)
- 
+
 
 #  Allow external access using host 0.0.0.0 address.
 if __name__ == '__main__':
