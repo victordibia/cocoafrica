5dea10a11cc5f21802314965639dceb43b1b1971
Victor Dibia <chuvidi2003@gmail.com>
2018-11-18 22:05:40 -0500

    search interface: add support for select all or individual select

--- utils/datastore.py
+++ utils/datastore.py
@@ -6,30 +6,35 @@ from google.cloud import datastore
 client = datastore.Client()
 
 # The kind for the new entity
-imagedata_kind = 'imagedata' 
+imagedata_kind = 'imagedata'
 
 
 def get_images():
-    query = client.query(kind=imagedata_kind)  
+    query = client.query(kind=imagedata_kind)
     query_results = list(query.fetch())
     return query_results
 
+
 def delete_image_data():
     print("df")
+
 
 def save_image_data(image_info, search_tags):
     # Prepares the new entity
     imagedata = datastore.Entity(client.key(imagedata_kind))
     imagedata['description'] = image_info["photo"]["title"]["_content"]
-    imagedata['url'] = "https://farm" + str(image_info["photo"]["farm"]) + ".staticflickr.com/" + image_info["photo"]["server"] +"/"+image_info["photo"]["id"] + "_" + image_info["photo"]["secret"] + "_c.jpg"   
+    imagedata['url'] = "https://farm" + str(image_info["photo"]["farm"]) + ".staticflickr.com/" + \
+        image_info["photo"]["server"] + "/"+image_info["photo"]["id"] + \
+        "_" + image_info["photo"]["secret"] + "_c.jpg"
     imagedata['source'] = 'flickr'
     imagedata['sourceid'] = image_info["photo"]["id"]
     imagedata['searchtags'] = search_tags
+    imagedata['searchterm'] = search_tags["searchterm"]
     imagedata['imageinfo'] = image_info
-    # Saves the entity 
+    # Saves the entity
     client.put(imagedata)
     return "data saved"
- 
+
 
 # # save_image_data()
 # get_all_images()
