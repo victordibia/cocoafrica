b8f67ed9a86e702908d0cb93537e1e72fe52aa5b
Victor Dibia <victor.dibia@gmail.com>
2019-05-27 13:45:54 -0400

    initial support for local saving of data and json logs

--- utils/datastore.py
+++ utils/datastore.py
@@ -39,6 +39,25 @@ def save_image_data(curated_data):
         # break
     return "data saved"
 
+def save_image_data_to_folder(curated_data):
+    for image_info in curated_data["imageids"]:
+        imagedata = datastore.Entity(client.key(imagedata_kind))
+        imagedata['title'] = image_info["title"]
+        imagedata['url'] = image_info["url"]
+        # imagedata['url'] = "https://farm" + str(image_info["photo"]["farm"]) + ".staticflickr.com/" + \
+        #     image_info["photo"]["server"] + "/"+image_info["photo"]["id"] + \
+        #     "_" + image_info["photo"]["secret"] + "_c.jpg"
+        imagedata['searchsource'] = curated_data["searchsource"]
+        imagedata['sourceid'] = image_info["id"]
+        imagedata['searchtags'] = curated_data["searchtags"]
+        imagedata['searchterm'] = curated_data["searchterm"]
+
+        # Saves the entity
+        # print(imagedata)
+        # client.put(imagedata)
+        # break
+    return "data saved"
+
 
 # # save_image_data()
 # get_all_images()
